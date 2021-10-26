61bdfbf8c9d19af2d699d529980bf4b3990e71e4
regent88 <il9.korolkov@gmail.com>
2021-10-27 00:39:20 +0500

    class Director and interface IManager added

---
+++ src/main/java/HW_17_1/Director.java
@@ -0,0 +1,9 @@
+package HW_17_1;
+
+public class Director extends BaseEmployee {
+
+
+    public Director(String name, int age, char gender, int getDailyRate) {
+        super(name, age, gender, getDailyRate);
+    }
+}
